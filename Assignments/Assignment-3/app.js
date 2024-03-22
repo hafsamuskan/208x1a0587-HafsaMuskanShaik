56b6e9f69caad5bc88d6aea68459234abf646cfa
SHAIK HAFSA MUSAKN <116863515+hafsamuskan@users.noreply.github.com>
2024-03-22 23:12:34 +0530

    Add files via upload

---
+++ Assignments/Assignment-3/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+require("dotenv/config");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+//Import routes 
+const postRoute = require('./routes/posts');
+
+//middleware
+app.use("/posts", postRoute);
+app.get('/', (req,res) => {
+    res.send("I'm inside a home!...");
+});
+
+// Connect the MongoDB
+mongoose.connect(process.env.DB_CONNECTION)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Error connecting to MongoDB:', err));
+
+//create a listening port
+app.listen(4000);
